84ee53a6384be7f63476e65701b0155aa7c187f8
N0amG <noamguez0@gmail.com>
2024-04-17 14:51:50 +0200

    dev boss

--- script/game.py
+++ script/game.py
@@ -100,7 +100,7 @@ class Game:
         #self.game_entities_list.append(turret.Plasma_Turret(self, self.matrice_tourelle[2][1][1], self.matrice_tourelle[2][1][0]))
         #[self.game_entities_list.append(turret.Basic_Turret(self ,self.matrice_tourelle[2][i][1], self.matrice_tourelle[2][i][0])) for i in range(4,5)]
 
-        self.bot_wave_spawner.manual_spawn(self.matrice_bot[3][0][1], self.matrice_bot[1][0][0], "titan")
+        self.bot_wave_spawner.manual_spawn(self.matrice_bot[3][0][1], self.matrice_bot[1][0][0], "ender")
         
         #self.bot_wave_spawner.manual_spawn(self.matrice_bot[2][1][1], self.matrice_bot[2][1][0], "basic")
         self.debug_bot_timer = None
